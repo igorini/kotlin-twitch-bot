4d4359effadc38f70b059d87e119007e2f713390
Igor Maguskin <maguskin.igor@gmail.com>
2018-03-07 13:17:04 +0000

    Add message handler to Bot

--- src/main/kotlin/com/igorini/kotlintwitchbot/Bot.kt
+++ src/main/kotlin/com/igorini/kotlintwitchbot/Bot.kt
@@ -1,10 +1,21 @@
 package com.igorini.kotlintwitchbot
 
+import com.cavariux.twitchirc.Chat.Channel
+import com.cavariux.twitchirc.Chat.User
 import com.cavariux.twitchirc.Core.TwitchBot
+import mu.KotlinLogging
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberFunctions
 
 /** Represents a Twitch Bot */
 open class Bot : TwitchBot() {
-    init {
-        //javaClass.methods!!.filter { it.isAnnotationPresent() }
+    open val logger = KotlinLogging.logger {}
+
+    val regexToMethod = this::class.memberFunctions
+            .filter { it.annotations.any { it is Trigger } }
+            .associate { it.findAnnotation<Trigger>()!!.regex.toRegex() to it }
+
+    override fun onMessage(user: User?, channel: Channel?, message: String?) {
+        regexToMethod.filterKeys { it.matches(message!!) }.values.forEach { it.call(user!!, channel!!, message!!) }
     }
 }

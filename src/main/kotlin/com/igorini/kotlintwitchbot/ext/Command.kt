e56a1837110dba3a4e6d534e3312ffd6c38f62b0
Igor Maguskin <maguskin.igor@gmail.com>
2018-03-16 01:27:07 +0000

    Log all viewers

--- src/main/kotlin/com/igorini/kotlintwitchbot/ext/Command.kt
+++ src/main/kotlin/com/igorini/kotlintwitchbot/ext/Command.kt
@@ -2,13 +2,20 @@ package com.igorini.kotlintwitchbot.ext
 
 import me.philippheuer.twitch4j.events.event.irc.ChannelMessageEvent
 import me.philippheuer.twitch4j.message.commands.Command
+import mu.KLogger
+import mu.KotlinLogging
 
 /* Contains extension functions and properties for [Command] */
+
+val Command.logger: KLogger
+    get() = KotlinLogging.logger {}
 
 /** @return Viewers that are online in this channel. */
 fun Command.viewers(messageEvent: ChannelMessageEvent): List<String> {
     val chatter = twitchClient.tmiEndpoint.getChatters(messageEvent.channel.name)
-    return chatter.viewers + chatter.admins + chatter.globalMods + chatter.moderators + chatter.staff
+    val viewers = chatter.viewers + chatter.admins + chatter.globalMods + chatter.moderators + chatter.staff
+    logger.info { "All viewers: $viewers" }
+    return viewers
 }
 
 /** @return A random viewer that is online in this channel, except provided users. */

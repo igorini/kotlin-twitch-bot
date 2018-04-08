4f83d86783a57916927dc8d04baa3e04ab41f6cb
Igor Maguskin <maguskin.igor@gmail.com>
2018-04-08 14:20:50 +0100

    Add caching to channel viewer lists

--- src/main/kotlin/com/igorini/kotlintwitchbot/TwitchBot.kt
+++ src/main/kotlin/com/igorini/kotlintwitchbot/TwitchBot.kt
@@ -4,24 +4,43 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import mu.KotlinLogging
 import com.fasterxml.jackson.module.kotlin.*
+import com.github.benmanes.caffeine.cache.Caffeine
+import com.github.benmanes.caffeine.cache.LoadingCache
 import me.philippheuer.twitch4j.TwitchClientBuilder
 import me.philippheuer.twitch4j.message.commands.Command
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 /** Represents a Twitch Bot */
 open class TwitchBot {
 
+    companion object {
+        val logger = KotlinLogging.logger {}
+
+        val configuration: Configuration = ObjectMapper(YAMLFactory()).registerKotlinModule().readValue(File(ClassLoader.getSystemResource("config.yaml").file))
+
+        val twitchClient = TwitchClientBuilder.init()
+                .withClientId(configuration.api["twitch_client_id"])
+                .withClientSecret(configuration.api["twitch_client_secret"])
+                .withCredential(configuration.credentials["irc"])
+                .withAutoSaveConfiguration(true)
+                .withConfigurationDirectory(File("config"))
+                .connect()
+
+        val viewersCache: LoadingCache<String, List<String>> = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterWrite(3, TimeUnit.MINUTES)
+                .build { viewers(it) }
+
+        private fun viewers(channel: String): List<String> {
+            val chatter = twitchClient.tmiEndpoint.getChatters(channel)
+            val viewers = chatter.viewers + chatter.admins + chatter.globalMods + chatter.moderators + chatter.staff
+            logger.info { "All viewers: $viewers" }
+            return viewers
+        }
+    }
+
     open val logger = KotlinLogging.logger {}
-
-    val configuration: Configuration = ObjectMapper(YAMLFactory()).registerKotlinModule().readValue(File(ClassLoader.getSystemResource("config.yaml").file))
-
-    val twitchClient = TwitchClientBuilder.init()
-            .withClientId(configuration.api["twitch_client_id"])
-            .withClientSecret(configuration.api["twitch_client_secret"])
-            .withCredential(configuration.credentials["irc"])
-            .withAutoSaveConfiguration(true)
-            .withConfigurationDirectory(File("config"))
-            .connect()
 
     init {
         // Register class to receive events with @EventSubscriber
--- src/main/kotlin/com/igorini/kotlintwitchbot/ext/Command.kt
+++ src/main/kotlin/com/igorini/kotlintwitchbot/ext/Command.kt
@@ -1,5 +1,6 @@
 package com.igorini.kotlintwitchbot.ext
 
+import com.igorini.kotlintwitchbot.TwitchBot
 import me.philippheuer.twitch4j.events.event.irc.ChannelMessageEvent
 import me.philippheuer.twitch4j.message.commands.Command
 import mu.KLogger
@@ -11,15 +12,10 @@ val Command.logger: KLogger
     get() = KotlinLogging.logger {}
 
 /** @return Viewers that are online in this channel. */
-fun Command.viewers(messageEvent: ChannelMessageEvent): List<String> {
-    val chatter = twitchClient.tmiEndpoint.getChatters(messageEvent.channel.name)
-    val viewers = chatter.viewers + chatter.admins + chatter.globalMods + chatter.moderators + chatter.staff
-    logger.info { "All viewers: $viewers" }
-    return viewers
-}
+fun Command.viewers(messageEvent: ChannelMessageEvent) = TwitchBot.viewersCache[messageEvent.channel.name]
 
 /** @return A random viewer that is online in this channel, except provided users. */
-fun Command.randomViewerExcept(messageEvent: ChannelMessageEvent, users: List<String>) = viewers(messageEvent).map { it.toLowerCase() }.minus(users).shuffled().firstOrNull()
+fun Command.randomViewerExcept(messageEvent: ChannelMessageEvent, users: List<String>) = viewers(messageEvent)?.map { it.toLowerCase() }?.minus(users)?.shuffled()?.firstOrNull()
 
 /** Checks if provided viewer is online, except provided users. */
-fun Command.viewerOnlineExcept(messageEvent: ChannelMessageEvent, viewer: String, users: List<String>) = viewers(messageEvent).map { it.toLowerCase() }.minus(users).contains(viewer)
+fun Command.viewerOnlineExcept(messageEvent: ChannelMessageEvent, viewer: String, users: List<String>) = viewers(messageEvent)?.map { it.toLowerCase() }?.minus(users)?.contains(viewer) ?: false
